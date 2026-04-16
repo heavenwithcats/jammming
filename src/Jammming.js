f3bcead3f82d195721050db89afa1b9c507bbde0
frogcalypso <frogcalypso@gmail.com>
2026-04-16 12:08:33 -0600

    commiting changes to internal logic such as creating new components

---
+++ src/Jammming.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { THE_SPOTIFY_CLIENT_SECRET, THE_SPOTIFY_CLIENT_ID, THE_SPOTIFY_REDIRECT_URL} from './Jammming.env';
+
+const App = () => {
+  
+} 
